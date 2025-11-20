b9414fb93520488e7281985139beec0ce9777d85
Okechukwu Victor <obinnaokechukeu750@gmail.com>
2025-11-20 16:19:58 +0100

    2.0

--- pages/Auth/forgotPassword.vue
+++ pages/Auth/forgotPassword.vue
@@ -1,5 +1,4 @@
 <template>
-  <div class="hidden md:block">
     <div
       class="w-screen min-h-screen bg-blue-100 flex items-center justify-center"
     >
@@ -47,48 +46,6 @@
         </div>
       </div>
     </div>
-  </div>
-
-  <div class="block md:hidden">
-    <div class="w-lvh min-h-screen flex justify-center">
- 
-      <div class="h-[324px] w-[375px] flex flex-col gap-[32px] mt-5">
-        <div class="flex items-center justify-center">
-          <img
-            src="/assets/icons/blue.png"
-            alt=""
-            class="w-[83px] h-[24px]"
-          />
-        </div>
-        <div class="bg-white px-[20px] rounded-md h-[268px] flex flex-col gap-[32px]">
-          <div class="h-[80px] w-[335px] flex flex-col justify-between">
-            <p class="font-semibold text-center text-[20px] bricolage">
-              Forgot Password ?
-            </p>
-            <p class="text-center text-gray-500 text-[16px] leading-[24px]">
-              Enter the email linked to your Zenly account <br />
-              and we'd send a link to reset your Password
-            </p>
-          </div>
-          <div class="w-[335px] h-[152px] flex flex-col gap-[32px]">
-            <div class="">
-              <p class="text-[16px] font-semibold">Work Email</p>
-              <input
-                type="email"
-                class="border w-[335px] p-3 text-sm rounded-lg h-[56px]"
-                placeholder="ovo@company.com"
-              />
-            </div>
-            <button
-              class="font-semibold text-xs bg-[#2F7DD0] h-[40px]  w-[335px] text-center text-white rounded-md"
-            >
-              Continue
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
 <script setup>
 definePageMeta({
--- pages/Auth/resetPassword.vue
+++ pages/Auth/resetPassword.vue
@@ -1,92 +1,89 @@
 <template>
-  <div class="hidden md:block">
-    <div
-      class="w-screen min-h-screen bg-blue-100 flex items-center justify-center"
-    >
-      <div class="flex flex-col gap-[32px] h-[526px] w-[456px]">
-        <div class="flex items-center justify-center">
-          <img
-            src="/assets/icons/Logo frame.png"
-            alt=""
-            class="w-[142px] h-[40px]"
-          />
-        </div>
-        <div class="bg-white rounded-md h-[454px] py-[32px] px-[28px]">
-          <div class="h-[390px] w-[400px] flex flex-col gap-[32px]">
-            <div class="h-[94px] w-[400px] flex flex-col justify-between">
-              <p class="font-semibold text-center text-[24px] bricolage">
-                Reset Password
-              </p>
-              <p class="text-center text-gray-500 text-[16px] W-72">
-                Enter a new password that would be used to access your Zenly
-                account
-              </p>
-            </div>
-            <div class="h-[264px] w-[400px] flex flex-col gap-[32px]">
-              <div class="relative h-[80px] flex flex-col gap-[4px]">
-                <p class="text-[14px] font-semibold">New Password</p>
-                <input
-                  :type="showPassword ? 'text' : 'password'"
-                  class="border w-[400px] h-[56px] p-3 text-sm rounded-lg"
-                  placeholder="Enter Password"
-                />
-                <button
-                  type="button"
-                  @click="togglePassword"
-                  class="absolute right-3 top-2/4 text-gray-500 hover:text-gray-800"
-                >
-                  <img
-                    v-if="showPassword === true"
-                    src="/assets/icons/eye-password-show-svgrepo-com.svg"
-                    alt="Show"
-                    class="w-5"
-                  />
-                  <img
-                    v-else
-                    src="/assets/icons/eye-password-hide-svgrepo-com.svg"
-                    alt="Hide"
-                    class="w-5"
-                  />
-                </button>
-              </div>
-              <div class="relative h-[80px] flex flex-col gap-[4px]">
-                <p class="text-[14px] font-semibold">Confirm Password</p>
-                <input
-                  :type="showConfimPassword ? 'text' : 'password'"
-                  class="border w-[400px] h-[56px] p-3 text-sm rounded-lg"
-                  placeholder="Enter Password"
-                />
-                <button
-                  type="button"
-                  @click="toggleConfirmPassword"
-                  class="absolute right-3 top-2/4 text-gray-500 hover:text-gray-800"
-                >
-                  <img
-                    v-if="showConfimPassword === true"
-                    src="/assets/icons/eye-password-show-svgrepo-com.svg"
-                    alt="Show"
-                    class="w-5"
-                  />
-                  <img
-                    v-else
-                    src="/assets/icons/eye-password-hide-svgrepo-com.svg"
-                    alt="Hide"
-                    class="w-5"
-                  />
-                </button>
-              </div>
+  <div
+    class="w-screen min-h-screen bg-blue-100 flex items-center justify-center"
+  >
+    <div class="flex flex-col gap-[32px] h-[526px] w-[456px]">
+      <div class="flex items-center justify-center">
+        <img
+          src="/assets/icons/Logo frame.png"
+          alt=""
+          class="w-[142px] h-[40px]"
+        />
+      </div>
+      <div class="bg-white rounded-md h-[454px] py-[32px] px-[28px]">
+        <div class="h-[390px] w-[400px] flex flex-col gap-[32px]">
+          <div class="h-[94px] w-[400px] flex flex-col justify-between">
+            <p class="font-semibold text-center text-[24px] bricolage">
+              Reset Password
+            </p>
+            <p class="text-center text-gray-500 text-[16px] W-72">
+              Enter a new password that would be used to access your Zenly
+              account
+            </p>
+          </div>
+          <div class="h-[264px] w-[400px] flex flex-col gap-[32px]">
+            <div class="relative h-[80px] flex flex-col gap-[4px]">
+              <p class="text-[14px] font-semibold">New Password</p>
+              <input
+                :type="showPassword ? 'text' : 'password'"
+                class="border w-[400px] h-[56px] p-3 text-sm rounded-lg"
+                placeholder="Enter Password"
+              />
               <button
-                class="font-semibold text-[16px] bg-[#2F7DD0] w-[400px] h-[40px] text-center text-white rounded-md"
+                type="button"
+                @click="togglePassword"
+                class="absolute right-3 top-2/4 text-gray-500 hover:text-gray-800"
               >
-               Reset Password
+                <img
+                  v-if="showPassword === true"
+                  src="/eye-password-show-svgrepo-com.svg"
+                  alt="Show"
+                  class="w-5"
+                />
+                <img
+                  v-else
+                  src="/eye-password-hide-svgrepo-com.svg"
+                  alt="Hide"
+                  class="w-5"
+                />
               </button>
             </div>
+            <div class="relative h-[80px] flex flex-col gap-[4px]">
+              <p class="text-[14px] font-semibold">Confirm Password</p>
+              <input
+                :type="showConfimPassword ? 'text' : 'password'"
+                class="border w-[400px] h-[56px] p-3 text-sm rounded-lg"
+                placeholder="Enter Password"
+              />
+              <button
+                type="button"
+                @click="toggleConfirmPassword"
+                class="absolute right-3 top-2/4 text-gray-500 hover:text-gray-800"
+              >
+                <img
+                  v-if="showConfimPassword === true"
+                  src="/eye-password-show-svgrepo-com.svg"
+                  alt="Show"
+                  class="w-5"
+                />
+                <img
+                  v-else
+                  src="/eye-password-hide-svgrepo-com.svg"
+                  alt="Hide"
+                  class="w-5"
+                />
+              </button>
+            </div>
+            <button
+              class="font-semibold text-[16px] bg-[#2F7DD0] w-[400px] h-[40px] text-center text-white rounded-md"
+            >
+              Reset Password
+            </button>
           </div>
         </div>
       </div>
     </div>
   </div>
-
 </template>
 <script setup>
 definePageMeta({
